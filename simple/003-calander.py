613659b0289ceccf28d24b433adac84521d2d905
eziraa <ezratgab@gmail.com>
2024-02-12 00:41:12 +0300

    calender module

---
+++ simple/003-calander.py
@@ -0,0 +1,9 @@
+# there is python module to work on calander
+import calendar
+month = calendar.month(2024, 2)
+print(month)
+
+# and also there is a method to check weather it is leap year
+
+is_leap = calendar.isleap(2024)
+print(is_leap)  # True
