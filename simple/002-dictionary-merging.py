f83ac4c3932f6108a13e7cb7bed8b6941e473f57
eziraa <ezratgab@gmail.com>
2024-02-12 00:33:36 +0300

    merging  dcitionary

---
+++ simple/002-dictionary-merging.py
@@ -0,0 +1,20 @@
+# if we have to separate dictionaries how to merge those and have exaclty one dictioanry
+# First method using '|' operator
+
+author_data1 = {
+    'name': 'ezira',
+    'age': 22,
+    'gender': 'M'
+}
+author_data2 = {
+    'department': 'Software Engineering',
+    'Skills': ['Web Design', 'Web Developement', 'Data Analysis']
+}
+
+full_author_data1 = author_data1 | author_data2
+print(full_author_data1)
+
+# Method two by using '**' operator
+
+full_author_data2 = {**author_data1, **author_data2}
+print(full_author_data2)
